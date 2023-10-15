355c3b9459186448d9f8cce3c4117ecaf5ec4687
BernardoGomess <147990818+BernardoGomess@users.noreply.github.com>
2023-10-15 17:03:12 +0100

    Add files via upload

--- react/exercise3.3/submit.js
+++ react/exercise3.3/submit.js
@@ -1,29 +1,18 @@
-
-
-
-
-
-
-
-
-
-function getForm(form){
+function getForm(form) {
     let name = form.elements["name"].value;
     let phone = form.elements["phone"].value;
     let email = form.elements["email"].value;
     let date = form.elements["birthday"].value;
-    let male = form.elements["male"].value;
-    let female = form.elements["female"].value;
+    const gender = form.querySelector('input[name="gender"]:checked').value;
     let adress = form.elements["adress"].value;
-    let  masters = form.elements["masters"].value;
-    let bachelor = form.elements["bachelor"].value;
- 
+    const degree = document.getElementById('degree').value;
+    let sports = document.getElementById('sports');
+
+    // let masters = form.elements["masters"].value;
+    // let bachelor = form.elements["bachelor"].value;
 
 
+    alert(name + "\n" + phone + "\n" + email + "\n" + date + "\n" + gender + "\n" + adress + "\n" + degree + "\n" + sports.value);
 
-
-
-
-alert(name + phone + email+ birthday+ male+ female+ adress+ masters+ bachelor);
-
+    // alert(name + phone + email + date + birthday + male + female  + masters + bachelor);
 }
